bd35938a0852aeb92bb4cb15e88db31f41842450
Emi Anyakpor Jr <eanyakpor@gmail.com>
2025-09-04 01:18:01 -0700

    fix to ignore deployment issues on vercel

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // ✅ Don’t fail the production build because of ESLint errors
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
