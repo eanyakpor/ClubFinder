98fb11046dfdbd11584573ecf098f2af909787a1
epicfloe7 <114299368+epicfloe7@users.noreply.github.com>
2025-10-18 14:54:07 -0700

    Added a search bar without the backend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,6 +49,31 @@ export default async function Home() {
   return (
     <main className="mx-auto max-w-5xl p-8 text-black">
       <PreviewBanner />
+      
+
+<form  className="max-w-md mx-auto">
+      <label htmlFor="search-input" className="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
+
+      <div className="relative">
+        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+          <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+          </svg>
+        </div>
+
+        <input id="search-input"  type="search" 
+          className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg shadow-sm bg-white focus:ring-blue-500 focus:border-blue-500" required />
+
+        <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">
+          Search
+        </button>
+      </div>
+    </form>
+
+
+
+
+
       <section className="mb-12">
         <h1 className="mb-6 text-4xl font-extrabold tracking-tight">Upcoming Events</h1>
 
