4ac51513012cd4c85b8156bcd54513fd8c7ebcc1
DCabahug1 <150985638+DCabahug1@users.noreply.github.com>
2025-10-19 20:32:36 -0700

    Update NavigationButtons.tsx

--- src/app/components/Hero/components/NavigationButtons.tsx
+++ src/app/components/Hero/components/NavigationButtons.tsx
@@ -6,9 +6,9 @@ import { Button } from "@/components/ui/button";
 function NavigationButtons() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 items-center gap-2">
-      <Button variant={"outline"} className="bg-card text-card-foreground w-52">View Events</Button>
-      <Button variant={"outline"} className="bg-card text-card-foreground w-52">Club Info</Button>
       <Button variant={"outline"} className="bg-card text-card-foreground w-52">Analytics</Button>
+      <Button variant={"outline"} className="bg-card text-card-foreground w-52">View Events</Button>
+      <Button variant={"outline"} className="bg-card text-card-foreground w-52">Edit Club Info</Button>
     </div>
   );
 }
