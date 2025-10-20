8b31b3a9760c18671756547218a9907ff2b8efce
DCabahug1 <150985638+DCabahug1@users.noreply.github.com>
2025-10-19 20:48:16 -0700

    Update NavigationButtons.tsx

--- src/app/components/Hero/components/NavigationButtons.tsx
+++ src/app/components/Hero/components/NavigationButtons.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 
 function NavigationButtons() {
-  return (
+  return ( 
     <div className="grid grid-cols-1 md:grid-cols-3 items-center gap-2">
       <Button variant={"outline"} className="bg-card text-card-foreground w-52">Analytics</Button>
       <Button variant={"outline"} className="bg-card text-card-foreground w-52">View Events</Button>
